a9e5c962432b772ab365fdf2373c8f36812b1af1
Pkononsky <Pkononsky@yandex.ru>
2019-10-26 23:05:40 +0500

    еще раз исправлено добавление ; при запросе Покажи

--- pbql.js
+++ pbql.js
@@ -258,6 +258,9 @@ function addFindData(d, result, name) {
     if (d === 'почты') {
         result[name].push(phoneBook.get(name).emails.join(','));
     }
+    if (result[name].indexOf('', result[name].indexOf('') + 1) !== -1) {
+        result[name].pop();
+    }
 }
 
 function findData(names, data) {
